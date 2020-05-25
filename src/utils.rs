bd283184b3dd801ef534d17428b629c719e61747
Yuki Imai <yuki08281993@gmail.com>
2020-05-25 23:33:58 +0900

    util is_sorted

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,11 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     let mut rng = Pcg64Mcg::from_seed([0;16]);
     rng.sample_iter(&Standard).take(n).collect();
 }
+
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1]);
+}
